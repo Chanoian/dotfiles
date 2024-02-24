c2b2f6eeb7f9cd84ebf2fa60cadcc2926667c17d
Ara Chanoian <achanoia@redhat.com>
2024-02-24 12:15:43 -0500

    add bufferline

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,17 +1,23 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.cmd("set mouse=a")
+-- Map Ctrl+C to yank in normal mode
+vim.keymap.set("v", "<C-c>", '"*y')
+vim.keymap.set("v", "<C-v>", '"*p')
+vim.cmd("set mouse=v")
 vim.cmd("set cursorline")
 vim.cmd("set title")
 vim.cmd("set ruler")
 vim.cmd("set number")
 vim.cmd("set filetype=on")
+vim.opt.swapfile = false
 vim.g.mapleader = " "
 -- Set Tabby as the default terminal program
+
+-- ToggleTerm
 vim.keymap.set(
 	"n",
 	"<S-t>",
@@ -19,5 +25,24 @@ vim.keymap.set(
 	{ noremap = true, silent = true }
 )
 
+-- NvimTree
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeOpen<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>q", ":NvimTreeClose<CR>", { noremap = true, silent = true })
+
+-- Bufferline
+vim.opt.termguicolors = true
+vim.api.nvim_set_keymap("n", "<leader>bb", ":BufferLinePick<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>bc", ":BufferLinePickClose<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+	desc = "Resize nvim-tree if nvim window got resized",
+
+	group = vim.api.nvim_create_augroup("NvimTreeResize", { clear = true }),
+	callback = function()
+		local percentage = 15
+
+		local ratio = percentage / 100
+		local width = math.floor(vim.go.columns * ratio)
+		vim.cmd("tabdo NvimTreeResize " .. width)
+	end,
+})
