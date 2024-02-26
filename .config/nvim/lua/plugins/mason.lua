1db868727f2fcaebe42541ae9682c6a236e4a7fd
Ara Chanoian <achanoia@redhat.com>
2024-02-26 11:33:16 -0500

    fix the mp

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,29 +1,39 @@
 return {
-  {
-    'williamboman/mason.nvim',
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pylsp", "yamlls", "ansiblels", "gopls", "dockerls", "jsonls","terraformls"} })
-    end
-  },
-  {
-  'neovim/nvim-lspconfig',
-  config = function()
-    local lspconfig = require('lspconfig')
-    lspconfig.lua_ls.setup({})
-    lspconfig.pylsp.setup({})
-    lspconfig.jsonls.setup({})
-    lspconfig.yamlls.setup({})
-    lspconfig.ansiblels.setup({})
-    lspconfig.gopls.setup({})
-    lspconfig.dockerls.setup({})
-    lspconfig.terraformls.setup({})
-  end
-  }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pylsp",
+					"yamlls",
+					"ansiblels",
+					"gopls",
+					"dockerls",
+					"jsonls",
+					"terraformls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.pylsp.setup({})
+			lspconfig.jsonls.setup({})
+			lspconfig.yamlls.setup({})
+			lspconfig.ansiblels.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.dockerls.setup({})
+			lspconfig.terraformls.setup({})
+		end,
+	},
 }
