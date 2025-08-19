b0dd7903e8951dbdde374e4f9aa4f62fefad0421
Ara Chanoian <shanoian90@gmail.com>
2025-08-19 18:18:59 -0400

    add new samples

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -29,9 +29,27 @@ vim.api.nvim_set_keymap("v", "<leader>ya", '"+y', { noremap = true })
 vim.keymap.set(
     "n",
     "<S-t>",
-    ':ToggleTerm direction=float close_on_exit=true, hide_numbers=true, float_opts={border="curved"}<CR>',
+    ':ToggleTerm direction=horizontal close_on_exit=true, hide_numbers=true, float_opts={border="curved"}<CR>',
     { noremap = true, silent = true }
 )
+-- Shift+Down: open/focus terminal #1 at bottom
+vim.keymap.set("n", "<S-Down>", function()
+  vim.cmd("1ToggleTerm direction=horizontal")
+end, { desc = "Jump Down to ToggleTerm" })
+
+-- Shift+Up: go up to editor (from terminal insert mode)
+vim.keymap.set("t", "<S-Up>", [[<C-\><C-n><C-w>k]],
+  { desc = "Jump Up to Editor", silent = true })
+
+-- Shift+Up: go up to editor (from terminal normal mode)
+vim.keymap.set("n", "<S-Up>", "<C-w>k",
+  { desc = "Jump Up to Editor", silent = true })
+
+-- Shift+Down: go down into terminal (from editor)
+vim.keymap.set("n", "<S-Down>", "<C-w>j",
+  { desc = "Jump Down to Terminal", silent = true })
+
+
 
 -- NvimTree
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeOpen<CR>", { noremap = true, silent = true })
@@ -44,6 +62,16 @@ vim.api.nvim_set_keymap("n", "<leader>bb", ":BufferLinePick<CR>", { noremap = tr
 vim.api.nvim_set_keymap("n", "<leader>bc", ":BufferLinePickClose<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ba", ":BufferLineCloseOthers<CR>", { noremap = true, silent = true })
 
+-- Inline virtual text + nice float
+vim.diagnostic.config({
+  virtual_text = { spacing = 2, prefix = "●" },
+  float = { border = "rounded" },
+})
+
+-- Show float on hover (no keypress)
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  callback = function() vim.diagnostic.open_float(nil, { focus = false }) end,
+})
 
 vim.api.nvim_create_autocmd({ "VimResized" }, {
     desc = "Resize nvim-tree if nvim window got resized",
--- .zshrc
+++ .zshrc
@@ -13,7 +13,7 @@ export PATH="/opt/homebrew/bin:$PATH"
 export PATH=$HOME/bin:/usr/local/bin:$HOME/Library/Python/3.12/bin/:$PATH
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
-
+export PATH=$HOME/flutter/bin:$PATH
 # To Run Krew Plugins
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 alias neat="kubectl neat"
@@ -133,3 +133,25 @@ export NVM_DIR="$HOME/.nvm"
 
 
 . "$HOME/.local/bin/env"
+
+# Added by Windsurf
+export PATH="/Users/achanoia/.codeium/windsurf/bin:$PATH"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/opt/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+
+get_openai_key() { security find-generic-password -a "$USER" -s "OPENAI_API_KEY" -w; }
+export OPENAI_API_KEY="$(get_openai_key)"
