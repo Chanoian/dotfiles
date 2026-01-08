d9d2efb509e8893427b5f5ebf2d0072218134541
Ara Chanoian <shanoian90@gmail.com>
2026-01-08 10:04:43 -0500

    fix mason

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,82 +1,88 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "golangci_lint_ls",
-                    "bashls",
-                    "pylsp",
-                    "yamlls",
-                    "ansiblels",
-                    "gopls",
-                    "dockerls",
-                    "jsonls",
-                    "terraformls",
-                    "tflint",
-                },
-            })
-        end,
-    },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"golangci_lint_ls",
+					"bashls",
+					"pylsp",
+					"yamlls",
+					"ansiblels",
+					"gopls",
+					"dockerls",
+					"jsonls",
+					"terraformls",
+					"tflint",
+				},
+			})
+		end,
+	},
 
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-        },
-        event = "BufReadPre",
-        config = function()
-            require("mason").setup()
-            require("mason-lspconfig").setup()
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		event = "BufReadPre",
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup()
 
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- NEW Neovim 0.11+ API
-            vim.lsp.config("lua_ls", {
-                capabilities = capabilities,
-            })
+			-- NEW Neovim 0.11+ API
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+			})
 
-            vim.lsp.config("pylsp", {
-                capabilities = capabilities,
-            })
+			vim.lsp.config("pylsp", {
+				capabilities = capabilities,
+			})
 
-            vim.lsp.config("jsonls", {
-                capabilities = capabilities,
-            })
+			vim.lsp.config("jsonls", {
+				capabilities = capabilities,
+			})
 
-            vim.lsp.config("yamlls", {
-                capabilities = capabilities,
-            })
+			vim.lsp.config("yamlls", {
+				capabilities = capabilities,
+			})
 
-            vim.lsp.config("ansiblels", {
-                capabilities = capabilities,
-            })
+			vim.lsp.config("ansiblels", {
+				capabilities = capabilities,
+			})
 
-            vim.lsp.config("gopls", {
-                capabilities = capabilities,
-                settings = {
-                    gopls = {
-                        completeUnimported = true,
-                        usePlaceholders = true,
-                        analyses = {
-                            unusedparams = true,
-                        },
-                    },
-                },
-            })
+			vim.lsp.config("gopls", {
+				capabilities = capabilities,
+				settings = {
+					gopls = {
+						completeUnimported = true,
+						usePlaceholders = false,
+						analyses = {
+							unusedparams = true,
+						},
+						staticcheck = true,
+					},
+				},
+			})
 
-            vim.lsp.config("dockerls", {
-                capabilities = capabilities,
-            })
-        end,
-    }
+			vim.diagnostic.config({
+				virtual_text = false,
+				update_in_insert = false,
+				float = { border = "rounded" },
+			})
+			vim.lsp.config("dockerls", {
+				capabilities = capabilities,
+			})
+		end,
+	},
 }
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,53 +1,62 @@
 local wezterm = require("wezterm")
 
 return {
-    -- Font
-    font = wezterm.font_with_fallback({
-        "JetBrainsMono Nerd Font",
-    }),
-    font_size = 13.0,
+	-- Font
+	font = wezterm.font_with_fallback({
+		"JetBrainsMono Nerd Font",
+	}),
+	font_size = 13.0,
 
-    -- GPU acceleration (fastest)
-    front_end = "WebGpu",
-    max_fps = 120,
-    animation_fps = 120,
+	default_cursor_style = "SteadyBar",
+	cursor_blink_rate = 500,
 
-    -- Window appearance
-    window_decorations = "RESIZE",
-    window_padding = {
-        left = 8,
-        right = 8,
-        top = 8,
-        bottom = 8,
-    },
+	-- GPU acceleration (fastest)
+	front_end = "WebGpu",
+	max_fps = 120,
+	animation_fps = 120,
 
-    warn_about_missing_glyphs = false,
-    -- Transparency
-    window_background_opacity = 0.97,
-    text_background_opacity = 1.0,
+	-- Window appearance
+	-- window_decorations = "RESIZE",
+	window_decorations = "RESIZE|TITLE",
 
-    -- Scrollback
-    scrollback_lines = 10000,
+	window_padding = {
+		left = 8,
+		right = 8,
+		top = 16,
+		bottom = 8,
+	},
+	tab_bar_at_bottom = true,
+	warn_about_missing_glyphs = false,
+	-- Transparency
+	window_background_opacity = 0.97,
+	text_background_opacity = 1.0,
 
-    -- Colorscheme
-    -- color_scheme = "GruvboxDark",
-    -- color_scheme = "Gruvbox Dark",
-    color_scheme = "Oceanic-Next",
-    -- color_scheme = "Catppuccin Mocha",
-    -- color_scheme = "Kanagawa Dragon (Gogh)",
-    -- color_scheme = "Everforest Dark Hard (Gogh)",
-    -- color_scheme = "Everforest Dark (Gogh)",
-    -- color_scheme = "Tokyo Night",
-    enable_tab_bar = true,
+	-- Scrollback
+	scrollback_lines = 10000,
 
-    hide_tab_bar_if_only_one_tab = true,
-    use_fancy_tab_bar = true,
+	-- Colorscheme
+	-- color_scheme = "GruvboxDark",
+	-- color_scheme = "Gruvbox Dark",
+	color_scheme = "Oceanic-Next",
+	-- color_scheme = "Catppuccin Mocha",
+	-- color_scheme = "Kanagawa Dragon (Gogh)",
+	-- color_scheme = "Everforest Dark Hard (Gogh)",
+	-- color_scheme = "Everforest Dark (Gogh)",
+	-- color_scheme = "Tokyo Night",
+	enable_tab_bar = true,
 
-    -- Keybindings
-    keys = {
-        { key = "f", mods = "CTRL|CMD", action = wezterm.action.ToggleFullScreen },
-    },
+	disable_default_key_bindings = false,
+	keys = {
 
-    -- Ligatures
-    harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+		-- macOS copy/paste ONLY with CMD
+		{ key = "c", mods = "CMD", action = wezterm.action.CopyTo("Clipboard") },
+		{ key = "v", mods = "CMD", action = wezterm.action.PasteFrom("Clipboard") },
+
+		{ key = "t", mods = "CMD", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+		{ key = "n", mods = "CMD", action = wezterm.action.SpawnWindow },
+		{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+	},
+
+	hide_tab_bar_if_only_one_tab = true,
+	use_fancy_tab_bar = true,
 }
